567579e5f8965874091277b886e7c0974684de14
root (Deneir) <root@deneir>
2024-12-10 00:38:28 +0100

    internationalization of settings/home

---
+++ resources/lang/de/home.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    'account_settings' =>                                       'Konto-Einstellungen',
+    'change_profile_photo' =>                                   'Profilfoto ändern',
+    'select_a_profile_photo' =>                                 'Wähle dein Profilfoto',
+	'must_be_a_jpeg_or_png_max_avatar_size' =>                  'Muss jpeg oder png sein. Maximale Größe:',
+    'upload' =>                                                 'Hochladen',
+    'delete_profile_photo' =>                                   'Profilfoto löschen',
+    'name' =>                                                   'Name',
+    'your_name' =>                                              'Dein Name',
+    'website' =>                                                'Website',
+    'bio' =>                                                    'Bio',
+    'add_a_bio_here' =>                                         'Deine Biografie',
+    'language' =>                                               'Sprache',
+    'pronouns' =>                                               'Pronomen',
+    'select_pronouns' =>                                        'Wähle Pronomen',
+    'select_up_to_4_pronouns_that_will_appear_on_etc' =>        'Wähle bis zu 4 Pronomen, die auf deinem Profil angezeigt werden.',
+    'account_aliases' =>                                        'Konto-Alias',
+    'manage_account_alias' =>                                   'Verwalte dein Aliaskonto',
+    'to_move_from_another_account_to_this_one_first_etc' =>     'Um von einem anderen Konto hierher zu migrieren, musst du zuerst einen Alias anlegen',
+    'account_migrate' =>                                        'Konto-Migration',
+    'migrate_to_another_account' =>                             'Ziehe dein Konto um',
+    'to_redirect_this_account_to_a_different_one_etc' =>        'Um dieses Konto auf ein anderes umzuleiten (wo es unterstützt wird).',
+    'storage_usage' =>                                          'Speichernutzung',
+    'storage_used' =>                                           'Belegter Speicher',
+    'submit' =>                                                 'Absenden',
+    'are_you_sure_you_want_to_delete_your_profile_photo' =>     'Bist du sicher, dass du dein Profilfoto löschen möchtest?',
+    'error' =>                                                  'Error',
+    'an_error_occured_please_try_again_later' =>                'An error occured, please try again later',
+
+];
